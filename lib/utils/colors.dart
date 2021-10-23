aa121fdcdefc597a750dc9a0d2348128d5fd1b64
Zamorite <zamorite@gmail.com>
2021-10-23 19:14:42 +0400

    🎨 Update field color

--- lib/utils/colors.dart
+++ lib/utils/colors.dart
@@ -4,7 +4,7 @@ class SLCColors {
   static const Color black = Color(0xff010309);
   static const Color white = Color(0xfff9f8fc);
   static const Color darkBlue = Color(0xff2c3251);
-  static const Color lightBlue = Color(0xffb4b8cc);
+  static const Color lightBlue = Color(0xffb4b8cc); // 0xffF4F5FB
   static const Color grey = Color(0xff5a5b60);
   static const Color blue = Color(0xff6e718b);
 }
